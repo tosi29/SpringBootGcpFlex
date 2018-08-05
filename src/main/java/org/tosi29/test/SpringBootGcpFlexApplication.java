cbae0773e799f9159780798d26243e09706e59c8
tosi29 <tosi16@gmail.com>
2018-08-06 00:47:56 +0900

    Initial commit

---
+++ src/main/java/org/tosi29/test/SpringBootGcpFlexApplication.java
@@ -0,0 +1,12 @@
+package org.tosi29.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpFlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGcpFlexApplication.class, args);
+	}
+}
---
+++ src/test/java/org/tosi29/test/SpringBootGcpFlexApplicationTests.java
@@ -0,0 +1,25 @@
+package org.tosi29.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class SpringBootGcpFlexApplicationTests {
+
+	  @Autowired
+	  private TestRestTemplate template;
+
+	  @Test
+	  public void contextLoads() throws Exception {
+	  }
+
+}
